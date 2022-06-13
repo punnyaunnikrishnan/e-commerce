f05a1f617169baf2c37c748e8efb0501d3bd1153
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-13 11:51:00 +0530

    Header.js

---
+++ amazon/src/Header.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { useStateValue } from "./StateProvider";
+import { Link } from "react-router-dom";
+import { auth } from "./Firebase";
+
+function Header() {
+    const [{ cart, user }] = useStateValue();
+    console.log({ cart, user });
+    const handleAuthentication = () => {
+        if (user) {
+            auth.signOut();
+        }
+    };
+    return (
+        <div className="header">
+            <Link to="/">
+                <img
+                    className="header_logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                    alt="logo"
+                />
+            </Link>
+
+            <div className="header_search">
+                <input className="header_searchInput" type="text" />
+                <SearchIcon className="header_searchIcon" />
+            </div>
+
+            <div className="header_nav">
+                <Link to={!user && "/login"}>
+                    <div onClick={handleAuthentication} className="header_option">
+                        <span className="header_optionLineOne">
+                            Hello {user ? user.email : "Guest"}
+                        </span>
+                        <span className="header_optionLineTwo">
+                            {user ? "Sign Out" : "Sign In"}
+                        </span>
+                    </div>
+                </Link>
+
+                <Link to="/orders">
+                    <div className="header_option">
+                        <span className="header_optionLineOne">Returns</span>
+                        <span className="header_optionLineTwo">&Orders</span>
+                    </div>
+                </Link>
+
+                <div className="header_option">
+                    <span className="header_optionLineOne">Your</span>
+                    <span className="header_optionLineTwo">Prime</span>
+                </div>
+                <Link to="/checkout">
+                    <div className="header_optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="header_optionLineTwo header_basketCount">
+                            {cart?.length}
+                        </span>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
